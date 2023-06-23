0df03ffadb6fcebbbb27040d1226d4d2b7df81e9
Kubilay Topaktaş <119957098+kubilayytpkts@users.noreply.github.com>
2023-06-23 15:51:55 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace TCVatandaslıkSorgulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTCKN = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSoyisim = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxİsim = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDTarih = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(62, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TC Kimlik No :";
+            // 
+            // textBoxTCKN
+            // 
+            this.textBoxTCKN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxTCKN.Location = new System.Drawing.Point(184, 136);
+            this.textBoxTCKN.Name = "textBoxTCKN";
+            this.textBoxTCKN.Size = new System.Drawing.Size(178, 23);
+            this.textBoxTCKN.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(103, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Soyisim :";
+            // 
+            // textBoxSoyisim
+            // 
+            this.textBoxSoyisim.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxSoyisim.Location = new System.Drawing.Point(185, 210);
+            this.textBoxSoyisim.Name = "textBoxSoyisim";
+            this.textBoxSoyisim.Size = new System.Drawing.Size(177, 23);
+            this.textBoxSoyisim.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(128, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "İsim :";
+            // 
+            // textBoxİsim
+            // 
+            this.textBoxİsim.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxİsim.Location = new System.Drawing.Point(184, 173);
+            this.textBoxİsim.Name = "textBoxİsim";
+            this.textBoxİsim.Size = new System.Drawing.Size(178, 23);
+            this.textBoxİsim.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(56, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 23);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Doğum Tarihi :";
+            // 
+            // textBoxDTarih
+            // 
+            this.textBoxDTarih.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxDTarih.Location = new System.Drawing.Point(184, 247);
+            this.textBoxDTarih.Name = "textBoxDTarih";
+            this.textBoxDTarih.Size = new System.Drawing.Size(178, 23);
+            this.textBoxDTarih.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(169, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(199, 286);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(151, 26);
+            this.metroButton1.TabIndex = 3;
+            this.metroButton1.Text = "Sorgula";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 357);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxİsim);
+            this.Controls.Add(this.textBoxDTarih);
+            this.Controls.Add(this.textBoxSoyisim);
+            this.Controls.Add(this.textBoxTCKN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TC Kimlik Bilgileri Sorgulama Sistemi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTCKN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSoyisim;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxİsim;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxDTarih;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+
+namespace TCVatandaslıkSorgulama
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            long TCKN = long.Parse (textBoxTCKN.Text);
+            string İsim = textBoxİsim.Text;
+            string Soyisim = textBoxSoyisim.Text;
+            int DogumTarih =int.Parse (textBoxDTarih.Text);
+
+            TCdogrulamaService.KPSPublicSoapClient sorgu = new TCdogrulamaService.KPSPublicSoapClient();
+            bool DönenDeğer = sorgu.TCKimlikNoDogrula(TCKN, İsim, Soyisim, DogumTarih);
+
+            if (DönenDeğer == true)
+                MessageBox.Show("Girilen kimlik bilgileri dogrulandı","Kayıt Bulundu",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Girilen kimlik bilgileri dogrulanmadı !!!", "Kayıt Bulunamadı !!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+    }
+}
